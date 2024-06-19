fa31e69e63eadaecf0af24577d71ef84735486f8
Rio Imam Pradana <92615550+Xeyn701@users.noreply.github.com>
2024-06-19 22:53:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 document.getElementById('khodamForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
+
     fetch('khodamData.json')
         .then(response => response.json())
         .then(data => {
             const randomIndex = Math.floor(Math.random() * data.khodams.length);
             const khodam = data.khodams[randomIndex];
+
             const resultDiv = document.getElementById('result');
-            resultDiv.innerHTML = `<p>Nama Khodam: ${khodam.khodamName}</p>
+            resultDiv.innerHTML = `<p>Nama Khodam: ${khodam.name}</p>
                                    <p>Kekuatan: ${khodam.power}</p>
-                                   <p>Deskripsi: ${khodam.description}</p>`;
+                                   <p>Deskripsi: ${khodam.description}</p>
+                                   <p>Nama Anda: ${name}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            const resultDiv = document.getElementById('result');
+            resultDiv.innerHTML = '<p>Gagal mengambil data Khodam.</p>';
         });
 });
