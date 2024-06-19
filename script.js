49b1754b28f5c9d5cdaf7b23cfae951ea565a1be
Rio Imam Pradana <92615550+Xeyn701@users.noreply.github.com>
2024-06-19 20:57:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('khodamForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    fetch('khodamData.json')
+        .then(response => response.json())
+        .then(data => {
+            const randomIndex = Math.floor(Math.random() * data.khodams.length);
+            const khodam = data.khodams[randomIndex];
+            const resultDiv = document.getElementById('result');
+            resultDiv.innerHTML = `<p>Nama Khodam: ${khodam.khodamName}</p>
+                                   <p>Kekuatan: ${khodam.power}</p>
+                                   <p>Deskripsi: ${khodam.description}</p>`;
+        });
+});
